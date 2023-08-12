48b1c3174ab87885104ab5449c20e24576ab7843
Kerem Emir Şuşoğlu <kerememirsusoglu@hotmail.com>
2023-08-12 03:17:42 +0300

    Temel html
    Temel Html5 öğretici içerik

---
+++ temel html/islem.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="tr"> <!-- site dili  -->
+<head>
+    <meta charset="UTF-8"><!-- Türkçe Dil  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- responsive ölçeklendirilmesi -->
+    <title>TUM HTML TAGLARI</title> <!-- site başlığı  -->
+</head>
+<body>
+
+    <h1>Başlık  1</h1>
+    <h2>Başlık  2</h2>
+    <h3>Başlık  3</h3>
+    <h4>Başlık  4</h4>
+    <h5>Başlık  5</h5>
+    <h6>Başlık  6</h6>
+    <!-- paragraf yazısı --><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat, est praesentium. Ullam illo esse cumque,
+         velit repudiandaez <br> <!-- boşluk bırakır --> aspernatur eaque beatae facilis alias ratione libero commodi enim dolor, nihil neque deserunt.</p>
+    <hr> <!-- sayfa çizgisi çeker -->
+    <b>KALIN YAZI</b>
+    <br>
+    <i>İtalik yazı</i>
+    <br>
+    <ins> alta çizgi çeker </ins>
+    <br>
+    <del>ortaya çizgi çizer</del>
+    <br>
+<!-- sırasız listeleme -->
+    <ul>
+      <li> sırasız liste 1 </li>
+      <li> sırasız liste 2</li>
+      <li> sırasız liste 3</li>
+      <li> sırasız liste 4</li>
+    </ul>
+    <!--// -->
+<br>
+    <!-- sıralı listeleme -->
+    <ol>
+        <li> sıralı liste 1 </li>
+        <li> sıralı liste 2</li>
+        <li> sıralı liste 3</li>
+        <li> sıralı liste 4</li>
+    </ol>
+      <!--// -->
+
+      <img src="https://siyahekran.net/blackscreen-512-1.png" alt="siyah ekran"> <!-- src yerine ya bilgisayardan dosya yada internetten link alarak resmi verebilirsiniz alt kısmıda o resim için ne olduğunu açıklarısnız -->
+      <br>
+      
+      <a href="https://www.adidas.com.tr/"> Yönlendirme linki  </a>
+      <br>
+      <!-- a taginin target özellikleri -->
+      <a target="_blank" href="https://www.adidas.com.tr/"> blank Yönlendirme linki  </a>
+      <br>
+      <a target="_parent" href="https://www.adidas.com.tr/"> parent Yönlendirme linki  </a>
+      <br>
+      <a target="_self" href="https://www.adidas.com.tr/"> self Yönlendirme linki  </a>
+      <br>
+      <a target="_top" href="https://www.adidas.com.tr/"> top Yönlendirme linki  </a>
+
+      <!-- TABLO  -->
+      <br>
+      <br>
+      <table border="1">
+<tr>
+<th>Ad Soyad</th>
+<th>No</th>
+<th>Okul</th>
+</tr>
+<tr>
+<td>Ahmet </td>
+<td>142</td> <td>Anadolu Üniversitesi</td>
+</tr>
+<tr>
+<td>Mehmet </td>
+<td>147</td>
+<td>Anadolu Üniversitesi</td>
+</tr>
+<tr>
+<td>Burcu </td>
+<td>149</td> 
+<td>Anadolu Üniversitesi</td>
+</tr> 
+</table>
+<tr>
+<tr>
+    <br>
+
+    <form action="islem.php">  </form> <!-- kullanıcalardan veri alabilmek için form methodunu kullanırız action kısmına verilerin göndereciği php dosyasını belirleriz -->
+    </body>
+</body>
+</html> 
